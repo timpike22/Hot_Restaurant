627f53edac785895ea51aeb0ab2d1a2582e02677
tdsteph1 <tdsteph1@asu.edu>
2017-10-11 20:16:36 -0700

    Added Server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Home page
+app.get("/", function(req, res)
+{
+	res.sendFile(path.join(__dirname, "home.html"));
+});
+
+//Open the tables page
+app.get("/tables", function(req, res)
+{
+	res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+//Open the reserve
+app.get("/reserve", function(req, res)
+{
+	res.sendFile(path.join(__dirname, "reserve.html"));
+
+});
+
+/*
+homes.html
+tables.html
+reserve.html
+*/
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function()
+{
+  console.log("App listening on PORT " + PORT);
+});
+
+
